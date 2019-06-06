fc3ef0ad5f950bf1f6b98c323cff2c54b9646492
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2019-06-06 16:01:19 +0300

    Auctioneer

---
+++ src/Auctioneer.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Auctioneer {
+
+    public boolean solveMDD(List<MDDAgent> agents){
+        boolean foundSolution = false;
+        // TODO: 06-Jun-19
+        return foundSolution;
+    }
+}
