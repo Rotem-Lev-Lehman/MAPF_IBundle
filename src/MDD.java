078c80069ee5d838daef1671dd6bdf3d9bbe3ba6
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-06-10 15:13:50 +0300

    MDD_Builder and MDD, total_time + goal vertex

--- src/MDD.java
+++ src/MDD.java
@@ -1,34 +1,39 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class MDD{
     private Graph original_graph;
     private List<MDD_Edge> mdd_edges;
     private List<MDD_Vertex> mdd_vertexes;
     private MDD_Vertex start_MDD_vertex;
+    private MDD_Vertex goal_MDD_vertex;
     private double bidding_cost;
     private double MDD_cost;
+    private int total_time;
     //private Map<Integer, List<MDD_Vertex>>
 
-    public MDD(Graph original_graph, Vertex start_vertex, double bidding_cost) {
+    public MDD(Graph original_graph, double bidding_cost) {
         this.original_graph = original_graph;
         //this.start_MDD_vertex = new MDD_Vertex(start_vertex, this, 0);
         this.start_MDD_vertex = null;
+        this.goal_MDD_vertex = null;
         this.bidding_cost = bidding_cost;
         mdd_edges = new ArrayList<>();
         mdd_vertexes = new ArrayList<>();
         MDD_cost = 0;
+        total_time = 0;
     }
 
-    public MDD(Graph original_graph, Vertex start_vertex) {
+    public MDD(Graph original_graph) {
         this.original_graph = original_graph;
         //this.start_MDD_vertex = new MDD_Vertex(start_vertex, this, 0);
         this.start_MDD_vertex = null;
+        this.goal_MDD_vertex = null;
         bidding_cost = 0;
         mdd_edges = new ArrayList<>();
         mdd_vertexes = new ArrayList<>();
         MDD_cost = 0;
+        total_time = 0;
     }
 
     public double getBidding_cost() {
@@ -75,11 +80,27 @@ public class MDD{
         this.start_MDD_vertex = start_MDD_vertex;
     }
 
+    public MDD_Vertex getGoal_MDD_vertex() {
+        return goal_MDD_vertex;
+    }
+
+    public void setGoal_MDD_vertex(MDD_Vertex goal_MDD_vertex) {
+        this.goal_MDD_vertex = goal_MDD_vertex;
+    }
+
     public double getMDD_cost() {
         return MDD_cost;
     }
 
     public double getTotalCost(){
         return MDD_cost + bidding_cost;
+    }
+
+    public int getTotal_time() {
+        return total_time;
+    }
+
+    public void setTotal_time(int total_time) {
+        this.total_time = total_time;
     }
 }
--- src/MDD_Builder.java
+++ src/MDD_Builder.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class MDD_Builder {
 
-    public static MDD Build(Graph graph, Vertex start, List<Path> paths){
-        if(paths.size() == 0)
+    public static MDD Build(Graph graph, Vertex start, List<Path> paths) {
+        if (paths.size() == 0)
             return null;
-        MDD mdd = new MDD(graph, start);
-        MDD_Vertex mdd_start = new MDD_Vertex(start, mdd, 0,0);
+        MDD mdd = new MDD(graph);
+        MDD_Vertex mdd_start = new MDD_Vertex(start, mdd, 0, 0);
         HashMap<Vertex, MDD_Vertex> prev_vertexes = new HashMap<>();
         prev_vertexes.put(start, mdd_start);
 
@@ -15,47 +15,68 @@ public class MDD_Builder {
 
         int totalTime = paths.get(0).size();
 
-        for(Path path : paths){
-            if(path.size() != totalTime)
+        for (Path path : paths) {
+            if (path.size() != totalTime)
                 throw new InputMismatchException("All paths must have the same size");
         }
 
-        for(int i = 1; i < totalTime; i++){
+        for (int i = 1; i < totalTime; i++) {
             HashMap<Vertex, MDD_Vertex> curr_vertexes = new HashMap<>();
-            for(Path curr_path : paths){
-                Vertex prev_vertex = curr_path.get(i-1);
+            for (Path curr_path : paths) {
+                Vertex prev_vertex = curr_path.get(i - 1);
                 MDD_Vertex prev;// = new MDD_Vertex(prev_vertex,mdd,i-1,-1);
                 Vertex curr_vertex = curr_path.get(i);
                 //MDD_Vertex curr = new MDD_Vertex(curr_vertex, mdd, i, -1);
-                if(prev_vertexes.containsKey(prev_vertex)){
+                if (prev_vertexes.containsKey(prev_vertex)) {
                     prev = prev_vertexes.get(prev_vertex);
-                    if(!curr_vertexes.containsKey(curr_vertex)){
+                    if (!curr_vertexes.containsKey(curr_vertex)) {
                         double cost_until_me = prev.getCost_until_me();
-                        if(curr_vertex.equals(prev_vertex)){
+                        if (curr_vertex.equals(prev_vertex)) {
                             //stayed
                             cost_until_me += curr_vertex.getStay_cost();
-                        }
-                        else{
+                        } else {
                             Edge edge = prev_vertex.getEdgeTo(curr_vertex);
-                            if(edge == null)
+                            if (edge == null)
                                 throw new UnsupportedOperationException();
                             cost_until_me += edge.getTravel_cost();
                         }
 
                         MDD_Vertex curr_mdd_vertex = new MDD_Vertex(curr_vertex, mdd, i, cost_until_me);
-                        MDD_Edge mdd_edge = new MDD_Edge(prev,curr_mdd_vertex,mdd);
+                        MDD_Edge mdd_edge = new MDD_Edge(prev, curr_mdd_vertex, mdd);
                         prev.addMDD_Edge(mdd_edge);
                         mdd.addMDD_Vertex(curr_mdd_vertex);
                         mdd.addMDD_Edge(mdd_edge);
                         curr_vertexes.put(curr_vertex, curr_mdd_vertex);
                     }
-                }
-                else
+                } else
                     throw new UnsupportedOperationException();
             }
             prev_vertexes = curr_vertexes;
         }
+
+        if (prev_vertexes.size() != 1) {
+            throw new UnsupportedOperationException("There can be only 1 goal state");
+        }
+        mdd.setGoal_MDD_vertex(prev_vertexes.get(0));
+        mdd.setTotal_time(totalTime);
         return mdd;
+    }
+
+    public static void normalizeTotalTime(MDD mdd, int new_total_time) {
+        int current_mdd_time = mdd.getTotal_time();
+        if (current_mdd_time >= new_total_time)
+            throw new UnsupportedOperationException("The new time must be more then the existing total time");
+
+        MDD_Vertex prev = mdd.getGoal_MDD_vertex();
+        Vertex goal = prev.getOriginal_vertex();
+        for (int i = current_mdd_time; i < new_total_time; i++) {
+            MDD_Vertex curr = new MDD_Vertex(goal, mdd, prev.getTime() + 1, prev.getCost_until_me());
+            MDD_Edge edge = new MDD_Edge(prev, curr, mdd);
+            prev.addMDD_Edge(edge);
+            mdd.addMDD_Vertex(curr);
+            mdd.addMDD_Edge(edge);
+        }
+        mdd.setTotal_time(new_total_time);
     }
 
 }
