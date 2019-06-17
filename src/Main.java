14d3b880341be7441d907c3a6014461f23a57d89
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2019-06-17 16:28:14 +0300

    bounded read scenearios

--- src/Main.java
+++ src/Main.java
@@ -20,48 +20,27 @@ public class Main {
             System.out.println();
         }
         System.out.println("----------------------");
-        //test scenario
-        //List<Scenario> scenarios = Scenario_Reader.readScenarios(new File("resources/scenarios/aTest1"));
-        //List<Scenario> scenarios = Scenario_Reader.readScenarios(new File("resources/scenarios/aTest3Agents1"));
-        //List<Scenario> scenarios = Scenario_Reader.readScenarios(new File("resources/scenarios/aTest3Agents2"));
+
         List<Scenario> scenarios = Scenario_Reader.readScenarios(new File("resources/scenarios/ca_cave.map.scen"));
-
-        int breakpoint=5;
-
-        /*
-        AStarDeep astar = new AStarDeep();
-        List<Path> paths = astar.searchDeepening(scenarios.get(0).getAgents().get(0),4,6);
-        for(Path path : paths)
-        {
-            System.out.println("***************************************************");
-            path.printPath();
-        }
-        */
-        //*******************************************************************************************************************************
-
-        MDD_Scenario mdd_scenario = new MDD_Scenario(scenarios.get(57));
-        /*
-        MDD_Agent agent = mdd_scenario.getAgents().get(0);
-        agent.calculateFirstMDD();
-        System.out.println(agent.getMaxMDDTotalCost());
-        Bid bid = agent.MakeABid();
-        bid.Decline();
-        System.out.println(agent.getMaxMDDTotalCost());
-        bid.normalizeTotalTime(9);
-        System.out.println(agent.getMaxMDDTotalCost());
-        */
-        //*******************************************************************************************************************************
-
-        Auctioneer auctioneer = new Auctioneer();
-        if(auctioneer.solve(mdd_scenario.getAgents())){
-            for(MDD_Agent agent1 : mdd_scenario.getAgents()){
-                System.out.println("***************************************************************************");
-                agent1.getFinal_path().printPath();
+        for(int i=0;i<scenarios.size();i=i+1){
+            System.out.println(scenarios.get(i).getAgents().size());
+            System.out.print(i+" : ");
+            MDD_Scenario mdd_scenario = new MDD_Scenario(scenarios.get(i));
+            Auctioneer auctioneer = new Auctioneer();
+            int sum =0;
+            if(auctioneer.solve(mdd_scenario.getAgents())){
+                for(MDD_Agent agent1 : mdd_scenario.getAgents()){
+                    /*System.out.println("***************************************************************************");
+                    agent1.getFinal_path().printPath();*/
+                    sum+=agent1.getFinal_path().size();
+                }
+                System.out.println("sum of costs - " + sum);
+            }
+            else {
+                System.out.println("Solver failed");
             }
         }
-        else {
-            System.out.println("Solver failed");
-        }
+
 
     }
 }
